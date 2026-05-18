38cc2245fb1b030c682d61d205a5256369c90f1a
Erick Pitleski <pitleskie@gmail.com>
2026-05-18 17:07:29 -0300

    lkds

--- script.js
+++ script.js
@@ -2,21 +2,21 @@
 //       SCRIPT.JS
 // =========================
 
-// efeito aparecendo suave ao rolar a página
+// animação suave ao descer
 
 const elementos = document.querySelectorAll(
   ".texto, .imagem-lateral, .flip-card, .caixa-reflexao"
 );
 
-function aparecerNaTela() {
+function aparecerNaTela(){
 
   const alturaTela = window.innerHeight;
 
   elementos.forEach((elemento) => {
 
-    const distanciaTopo = elemento.getBoundingClientRect().top;
+    const distancia = elemento.getBoundingClientRect().top;
 
-    if (distanciaTopo < alturaTela - 100) {
+    if(distancia < alturaTela - 100){
 
       elemento.classList.add("mostrar");
 
@@ -31,7 +31,7 @@ window.addEventListener("scroll", aparecerNaTela);
 aparecerNaTela();
 
 
-// efeito leve no menu quando desce a página
+// efeito do menu
 
 const header = document.querySelector("header");
 
@@ -40,7 +40,7 @@ window.addEventListener("scroll", () => {
   if(window.scrollY > 50){
 
     header.style.background = "#f5f2ebee";
-    header.style.backdropFilter = "blur(6px)";
+    header.style.backdropFilter = "blur(5px)";
     header.style.boxShadow = "0 2px 10px rgba(0,0,0,.05)";
 
   } else {
